771a63d3c7741f8ae9f97e702717873bf1adefe4
LautaroBoasso <lautaroboasso59@gmail.com>
2023-01-10 13:38:50 -0300

    Botones

--- GSII.py
+++ GSII.py
@@ -10,4 +10,13 @@ etiqueta.pack(fill = tkinter.X)
 etiqueta2 = tkinter.Label(ventana, text = "Hecho por Lautaro Boasso", bg = "Orange")
 etiqueta2.pack(side = tkinter.BOTTOM)
 
+boton1 = tkinter.Button(ventana, padx= 4, pady= 5, text = "Ver Base de Datos",command = lambda: print ("Prueba"))
+boton1.pack()
+
+boton2 = tkinter.Button(ventana, padx= 3, pady= 4, text = "Agregar un nuevo registro",command = lambda: print ("Prueba"))
+boton2.pack()
+
+boton3 = tkinter.Button(ventana, padx= 3, pady= 4, text = "Modificar un Registro",command = lambda: print ("Prueba"))
+boton3.pack()
+
 ventana.mainloop()
