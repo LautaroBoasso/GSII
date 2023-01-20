0dae23bb9dd4deb8574cc4b80336c14b619ed7a7
LautaroBoasso <lautaroboasso59@gmail.com>
2023-01-20 04:11:19 -0300

    asd

---
+++ PracticasPython.py
@@ -0,0 +1,18 @@
+# Print
+
+print ("Hello World")
+print(type(10))
+print(type(1 + 2j))
+print(type("Hola"))
+print(type(10.5))
+
+# Variables
+
+nombre = "Lautaro"
+apellido = "Boasso"
+
+print(nombre, apellido)
+print("El nombre y apellido del usuario es: ",nombre,apellido)
+
+nombre, apellido, edad = "Lautaro", "Boasso", 24
+print ("Mi nombre es:", nombre, "mi apellido es: ", apellido, "Y tengo: " , edad, "años")
