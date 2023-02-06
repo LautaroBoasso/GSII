84a3dc1816349ba20dfc9accd8e305755949f6cb
LautaroBoasso <lautaroboasso59@gmail.com>
2023-02-06 14:48:22 -0300

    Parte lógica del Agregar Jugadores, pero no funciona

--- GSII.py
+++ GSII.py
@@ -1,7 +1,9 @@
 from tkinter import *
 import mariadb
 
+
 # Funciones
+
 
 def abrir_bdd():
     ventana_verbdd = Toplevel()
@@ -25,9 +27,9 @@ def agregar_registro():
     a_nombre = Entry(ventana_registro)
     a_nombre.grid(row = 1, column = 1)
 
-    agregar_registro_apellido = Label(ventana_registro, text= "Apellido").grid(row = 2, column = 0, pady=5)
-    a_apellido = Entry(ventana_registro)
-    a_apellido.grid(row = 2, column = 1)
+    agregar_registro_apellidos = Label(ventana_registro, text= "Apellido").grid(row = 2, column = 0, pady=5)
+    a_apellidos = Entry(ventana_registro)
+    a_apellidos.grid(row = 2, column = 1)
 
     agregar_registro_dorsal = Label(ventana_registro, text= "Dorsal").grid(row = 3, column = 0, pady=5)
     a_dorsal = Entry(ventana_registro)
@@ -41,15 +43,30 @@ def agregar_registro():
     a_posicion = Entry(ventana_registro)
     a_posicion.grid(row = 5, column= 1)
 
-    boton_cerrar_registro = Button(ventana_registro, text = "Cerrar", command=ventana_registro.destroy)
-    boton_cerrar_registro.grid(row = 6, column= 1)
+    boton_registrar = Button(ventana_registro, text = "Registrar", command=registro_jugador).grid(row = 6, column = 0, pady=5)
+
+    #boton_cerrar_registro = Button(ventana_registro, text = "Cerrar", command=ventana_registro.destroy)
+    #boton_cerrar_registro.grid(row = 6, column= 1)
+
+def registro_jugador():
+    nombre = a_nombre.get()
+    apellidos = a_apellidos.get()
+    dorsal = a_dorsal.get()
+    edad = a_edad.get()
+    posicion = a_posicion.get()
+
+    try:
+        cursor.execute("INSERT INTO jugadores (nombre, apellidos, dorsal, edad, posicion) VALUES (?,?,?,?,?)", (nombre, apellidos, dorsal, edad, posicion))
+        conexion.commit()
+
+    except mariadb.Error as error_registro:
+        print (f"Error al registrar los datos {error_registro}")
 
 # Ventana principal
 
 ventana_principal = Tk()
 ventana_principal.geometry("600x800")
 ventana_principal.title ("TP Final")
-
 
 # Base de Datos
 
@@ -63,9 +80,11 @@ try:
     )
     Label(ventana_principal, text = "Se conecto a la Base de Datos correctamente: " +  conexion.database + ".").pack(side = BOTTOM)
 
+    cursor = conexion.cursor()
+
 except mariadb.Error as error:
     print (f"Error al conectarse a la Base de Datos  {error}")
-
+    
 
 
 # Interfaz gráfica
