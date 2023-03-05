244bd5e322d021559b332ba210e61fecba9ad69d
Lautaro Boasso <lautaroboasso59@gmail.com>
2023-03-05 14:47:36 -0300

    Conexion a la BDD y Agregar Jugadores

--- CRUD GSII.py
+++ CRUD GSII.py
@@ -15,3 +15,53 @@ miApellidos= StringVar()
 miEdad= StringVar()
 miDorsal= StringVar()
 miPosicion= StringVar()
+
+# Base de Datos
+
+try:
+    conexion = mariadb.connect(
+    user= "root",
+    password="",
+    host="127.0.0.1",
+    port= 3307,
+    database = "mundial"
+    )
+    Label(root, text = "Se conecto a la Base de Datos correctamente: " +  conexion.database + ".").pack(side = BOTTOM)
+
+except mariadb.Error as error:
+    print (f"Error al conectarse a la Base de Datos  {error}")
+
+
+def SalirAplicacion():
+    valor=messagebox.askquestion("Salir","¿Cerrar aplicación?")
+    if valor=="yes":
+       root.destroy()
+
+
+def LimpiarCampos():
+    miID.set("")
+    miNombre.set("")
+    miApellidos.set("")
+    miEdad.set("")
+    miDorsal.set("")
+    miPosicion.set("")
+
+def Mensaje():
+    acerca= """
+    Aplicación CRUD
+    Hecha por Lautaro Boasso para Gestión de Software II
+    Con Python Tkinter
+    """
+
+# Desarrollo CRUD #
+
+def Crear():
+    cursor=conexion.cursor()
+
+    try:
+        datos=miNombre.get(),miApellidos.get(),miEdad.get(),miDorsal.get(),miPosicion.get()
+        cursor.execute("INSERT INTO jugadores VALUES (NULL,?,?,?,?,?)", (datos))
+        conexion.commit()
+
+    except mariadb.Error as error_registro:
+        print (f"Error al registrar los datos {error_registro}")
