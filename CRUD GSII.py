8cf2e93aaf52b36c7d95f1af21903181b6cb20c2
Lautaro Boasso <lautaroboasso59@gmail.com>
2023-03-17 04:59:37 -0300

    Entrys y Labels

--- CRUD GSII.py
+++ CRUD GSII.py
@@ -48,11 +48,13 @@ def LimpiarCampos():
     miPosicion.set("")
 
 def Mensaje():
-    acerca= """
-    Aplicación CRUD
+    acerca= """Aplicación CRUD
     Hecha por Lautaro Boasso para Gestión de Software II
     Con Python Tkinter
     """
+    messagebox.showinfo(title="INFORMACION", message=acerca)
+
+# Desarrollo CRUD #
 
 def actualizar():
     cursor=conexion.cursor()
@@ -65,7 +67,7 @@ def actualizar():
     except mariadb.Error as error_registro:
         print (f"Error al registrar los datos {error_registro}")
         pass
-    limpiarCampos()
+    LimpiarCampos()
     mostrar()
 
 def eliminar():
@@ -77,11 +79,9 @@ def eliminar():
     except:
         messagebox.showwarning("ADVERTENCIA","Ocurrió un error al tratar de borrar el registro")
         pass
-    limpiarCampos()
+    LimpiarCampos()
     mostrar()
 
-
-# Desarrollo CRUD #
 
 def Crear():
     cursor=conexion.cursor()
@@ -125,5 +125,45 @@ tree.heading('#4', text="Dorsal", anchor=CENTER)
 tree.column('#4', width=50)
 tree.heading('#5', text="Posición", anchor=CENTER)
 
+# WIDGETS #
+
+menubar=Menu(root)
+menubasedat=Menu(menubar,tearoff=0)
+menubasedat.add_command(label="Salir", command=SalirAplicacion)
+menubar.add_cascade(label="Inicio", menu=menubasedat)
+
+ayudamenu=Menu(menubar,tearoff=0)
+ayudamenu.add_command(label="Resetear Campos", command=LimpiarCampos)
+ayudamenu.add_command(label="Acerca", command=Mensaje)
+menubar.add_cascade(label="Ayuda",menu=ayudamenu)
+
+e1=Entry(root, textvariable=miID)
+
+l2=Label(root, text="Nombre")
+l2.place(x=80,y=10)
+e2=Entry(root, textvariable=miNombre, width=20)
+e2.place(x=135,y=10)
+
+l3=Label(root, text="Apellidos")
+l3.place(x=280,y=10)
+e3=Entry(root, textvariable=miApellidos, width=30)
+e3.place(x=335,y=10)
+
+l4=Label(root, text="Edad")
+l4.place(x=540,y=10)
+e4=Entry(root, textvariable=miEdad, width=5)
+e4.place(x=575,y=10)
+
+l5=Label(root, text="Dorsal")
+l5.place(x=230,y=40)
+e5=Entry(root, textvariable=miDorsal, width=5)
+e5.place(x=270,y=40)
+
+l6=Label(root, text="Posición")
+l6.place(x=400,y=40)
+e6=Entry(root, textvariable=miPosicion, width=25)
+e6.place(x=455,y=40)
+
+root.config(menu=menubar)
 
 root.mainloop()
