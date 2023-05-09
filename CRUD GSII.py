e8cf305bba2c2ec21d9078b91ff40563033e315c
Lautaro Boasso <lautaroboasso59@gmail.com>
2023-05-09 08:55:48 -0300

    Modificando el código para que tenga orientación a objetos

--- CRUD GSII.py
+++ CRUD GSII.py
@@ -1,88 +1,121 @@
 from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
-import mariadb
+import sqlite3
 
 # Ventana principal e Interfaz Gráfica
 
-root=Tk()
-root.title("Aplicación CRUD para GSII")
-root.geometry("1000x400")
+class Jugadores:
+    db_nombre = 'jugadores.db'
 
-miID= StringVar()
-miNombre= StringVar()
-miApellidos= StringVar()
-miEdad= StringVar()
-miDorsal= StringVar()
-miPosicion= StringVar()
+    def __init__(self):
+     self.root=Tk()
+     self.root.title("Aplicación CRUD para GSII")
+     self.root.geometry("1000x400")
 
-# Base de Datos
+     self.miID= StringVar()
+     self.miNombre= StringVar()
+     self.miApellidos= StringVar()
+     self.miEdad= StringVar()
+     self.miDorsal= StringVar()
+     self.miPosicion= StringVar()
+     
+     # ETIQUETAS Y CAJAS DE TEXTO #
 
-try:
-    conexion = mariadb.connect(
-    user= "root",
-    password="",
-    host="127.0.0.1",
-    port= 3307,
-    database = "mundial"
-    )
-    Label(root, text = "Se conecto a la Base de Datos correctamente: " +  conexion.database + ".").pack(side = BOTTOM)
+     e1=Entry(self.root, textvariable=self.miID)
 
-except mariadb.Error as error:
-    print (f"Error al conectarse a la Base de Datos  {error}")
+     l2=Label(self.root, text="Nombre")
+     l2.place(x=15,y=10)
+     self.Nombre=Entry(self.root, textvariable=self.miNombre, width=20)
+     self.Nombre.place(x=70,y=10)
 
-# FUNCIONES # 
+     l3=Label(self.root, text="Apellidos")
+     l3.place(x=215,y=10)
+     self.Apellidos=Entry(self.root, textvariable=self.miApellidos, width=30)
+     self.Apellidos.place(x=270,y=10)
 
-def SalirAplicacion():
-    valor=messagebox.askquestion("Salir","¿Cerrar aplicación?")
-    if valor=="yes":
-       root.destroy()
+     l4=Label(self.root, text="Edad")
+     l4.place(x=466,y=10)
+     self.Edad=Entry(self.root, textvariable=self.miEdad, width=5)
+     self.Edad.place(x=500,y=10)
+
+     l5=Label(self.root, text="Dorsal")
+     l5.place(x=165,y=40)
+     self.Dorsal=Entry(self.root, textvariable=self.miDorsal, width=5)
+     self.Dorsal.place(x=205,y=40)
+
+     l6=Label(self.root, text="Posición")
+     l6.place(x=325,y=40)
+     self.Posicion=Entry(self.root, textvariable=self.miPosicion, width=25)
+     self.Posicion.place(x=380,y=40)
+
+     self.e7=Entry(self.root, width=20)
+     self.e7.place(x=640,y=90)
+
+     # BOTONES #
+
+     boton_agregar=Button(self.root, text="Agregar Registro",bg="lightgreen", command=Crear)
+     boton_agregar.place(x=15, y=90)
+
+     boton_modificar=Button(self.root, text="Modificar Registro",bg="lightblue", command=actualizar)
+     boton_modificar.place(x=155, y=90)
+
+     boton_mostrar=Button(self.root, text="Mostrar Lista",bg="yellow", command=mostrar)
+     boton_mostrar.place(x=310, y=90)
+
+     boton_eliminar=Button(self.root, text="Eliminar Registro",bg="red", command=eliminar)
+     boton_eliminar.place(x=435, y=90)
+
+     boton_buscar=Button(self.root, text="Buscar", command=Buscar_jugadores)
+     boton_buscar.place(x=770, y=87)
+
+     # TABLA # 
+
+     self.tree=ttk.Treeview(height=10, columns=('#0', '#1', '#2', '#3', '#4'))
+     self.tree.place(x= 15, y= 130)
+     self.tree.column('#0', width=50)
+     self.tree.heading('#0', text="ID", anchor=CENTER)
+     self.tree.heading('#1', text="Nombre", anchor=CENTER)
+     self.tree.heading('#2', text="Apellidos", anchor=CENTER)
+     self.tree.heading('#3', text="Edad", anchor=CENTER)
+     self.tree.column('#3', width=50)
+     self.tree.heading('#4', text="Dorsal", anchor=CENTER)
+     self.tree.column('#4', width=50)
+     self.tree.heading('#5', text="Posición", anchor=CENTER)
 
 
-def LimpiarCampos():
-    miID.set("")
-    miNombre.set("")
-    miApellidos.set("")
-    miEdad.set("")
-    miDorsal.set("")
-    miPosicion.set("")
 
-def Mensaje():
-    acerca= """Aplicación CRUD
-    Hecha por Lautaro Boasso para Gestión de Software II
-    Con Python Tkinter
-    """
-    messagebox.showinfo(title="INFORMACION", message=acerca)
+     # FUNCIONES # 
 
-def MensajeBDD():
-    acerca= """La Base de Datos fue creada con la siguiente sintaxis: 
-
-CREATE TABLE jugadores 
-(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-nombre VARCHAR(32) NOT NULL, 
-apellidos VARCHAR(64) NOT NULL, 
-edad VARCHAR(2),
-dorsal VARCHAR(3),  
-posicion VARCHAR (25) NOT NULL)
-    """
-    messagebox.showinfo(title="INFORMACION", message=acerca)
+ def SalirAplicacion(self):
+     valor=messagebox.askquestion("Salir","¿Cerrar aplicación?")
+     if valor=="yes":
+            self.root.destroy()
 
 
-# Desarrollo CRUD #
+ def LimpiarCampos(self):
+     self.miID.set("")
+     self.miNombre.set("")
+     self.miApellidos.set("")
+     self.miEdad.set("")
+     self.miDorsal.set("")
+     self.miPosicion.set("")
 
-def actualizar():
-    cursor=conexion.cursor()
 
-    try:
-        datos=miNombre.get(),miApellidos.get(),miEdad.get(),miDorsal.get(),miPosicion.get()
-        cursor.execute("UPDATE jugadores SET nombre=?, apellidos=?, edad=?, dorsal=?, posicion=? WHERE id="+miID.get(), (datos))
-        conexion.commit()
+ # Desarrollo CRUD #
 
-    except mariadb.Error as error_registro:
-        print (f"Error al registrar los datos {error_registro}")
-        pass
-    LimpiarCampos()
-    mostrar()
+ def actualizar(self):
+
+ try:
+     datos=self.miNombre.get(),self.miApellidos.get(),self.miEdad.get(),self.miDorsal.get(),self.miPosicion.get()
+     cursor.execute("UPDATE jugadores SET nombre=?, apellidos=?, edad=?, dorsal=?, posicion=? WHERE id="+miID.get(), (datos))
+     conexion.commit()
+
+     except mariadb.Error as error_registro:
+     print (f"Error al registrar los datos {error_registro}")
+     pass
+ LimpiarCampos()
+ mostrar()
 
 def eliminar():
     cursor=conexion.cursor()
@@ -124,21 +157,7 @@ def mostrar():
             tree.insert("",0,text=row[0], values=(row[1],row[2],row[3],row[4],row[5]))
     except:
         pass
-    
-    
-    # TABLA # 
 
-tree=ttk.Treeview(height=10, columns=('#0', '#1', '#2', '#3', '#4'))
-tree.place(x= 15, y= 130)
-tree.column('#0', width=50)
-tree.heading('#0', text="ID", anchor=CENTER)
-tree.heading('#1', text="Nombre", anchor=CENTER)
-tree.heading('#2', text="Apellidos", anchor=CENTER)
-tree.heading('#3', text="Edad", anchor=CENTER)
-tree.column('#3', width=50)
-tree.heading('#4', text="Dorsal", anchor=CENTER)
-tree.column('#4', width=50)
-tree.heading('#5', text="Posición", anchor=CENTER)
 
 def SeleccionarUsandoClick(event):
     item=tree.identify('item',event.x,event.y)
@@ -151,36 +170,36 @@ def SeleccionarUsandoClick(event):
 
 tree.bind("<Double-1>", SeleccionarUsandoClick)
 
-def Buscar_jugadores(self):
+def Buscar_jugadores():
         #Obtener todos los elementos con get_children(), que retorna una tupla de ID.
-        records=self.tree.get_children()
+        records=tree.get_children()
         for element in records:
-            self.tree.delete(element)
+            tree.delete(element)
         
-        if (self.combo_buscar.get()=='Nombre'):
+        if (combo_buscar.get()=='Nombre'):
             query=("SELECT * FROM jugadores WHERE Nombre LIKE ? ") 
-            parameters=(self.e7.get()+"%")
-            db_rows=self.Ejecutar_consulta(query,(parameters,))
+            parameters=("%"+e7.get()+"%")
+            db_rows=Ejecutar_consulta(query,(parameters,))
             for row in db_rows:
-                self.tree.insert("",0, text=row[1],values=(row[2],row[3],row[4],row[5],row[6]))
-            if(list(self.tree.get_children())==[]):
+                tree.insert("",0,text=row[0], values=(row[1],row[2],row[3],row[4],row[5]))
+            if(list(tree.get_children())==[]):
                messagebox.showerror("ERROR","Jugador no encontrado")
         else:
             query=("SELECT * FROM jugadores WHERE Apellidos LIKE ? ")
-            parameters=("%"+self.e7.get()+"%")
-            db_rows=self.Ejecutar_consulta(query,(parameters,))
+            parameters=("%"+e7.get()+"%")
+            db_rows=Ejecutar_consulta(query,(parameters,))
             for row in db_rows:
-                self.tree.insert("",0, text=row[1],values=(row[2],row[3],row[4],row[5],row[6]))
-            if(list(self.tree.get_children())==[]):
+                tree.insert("",0, text=row[1],values=(row[2],row[3],row[4],row[5],row[6]))
+            if(list(tree.get_children())==[]):
                messagebox.showerror("ERROR","Producto no encontrado")
 
-def Ejecutar_consulta(self, query, parameters=()):
-        with mariadb.connect:
-            cursor=conexion.cursor()
-            result=cursor.execute(query,parameters)
-            conexion.commit()
-        return result  
-
+def Ejecutar_consulta(query, parameters=()):
+        
+        cursor=conexion.cursor()
+        result=cursor.execute(query,parameters)
+        conexion.commit()
+        return result
+        
  
 
 # WIDGETS #
@@ -203,55 +222,11 @@ combo_buscar.current(0)
 combo_buscar.place(x=640, y=60)
 
 
-# ETIQUETAS Y CAJAS DE TEXTO #
 
-e1=Entry(root, textvariable=miID)
 
-l2=Label(root, text="Nombre")
-l2.place(x=15,y=10)
-e2=Entry(root, textvariable=miNombre, width=20)
-e2.place(x=70,y=10)
 
-l3=Label(root, text="Apellidos")
-l3.place(x=215,y=10)
-e3=Entry(root, textvariable=miApellidos, width=30)
-e3.place(x=270,y=10)
 
-l4=Label(root, text="Edad")
-l4.place(x=466,y=10)
-e4=Entry(root, textvariable=miEdad, width=5)
-e4.place(x=500,y=10)
-
-l5=Label(root, text="Dorsal")
-l5.place(x=165,y=40)
-e5=Entry(root, textvariable=miDorsal, width=5)
-e5.place(x=205,y=40)
-
-l6=Label(root, text="Posición")
-l6.place(x=325,y=40)
-e6=Entry(root, textvariable=miPosicion, width=25)
-e6.place(x=380,y=40)
-
-e7=Entry(root, width=20)
-e7.place(x=640,y=90)
-
-# BOTONES #
-
-b1=Button(root, text="Agregar Registro",bg="lightgreen", command=Crear)
-b1.place(x=15, y=90)
-
-b2=Button(root, text="Modificar Registro",bg="lightblue", command=actualizar)
-b2.place(x=155, y=90)
-
-b3=Button(root, text="Mostrar Lista",bg="yellow", command=mostrar)
-b3.place(x=310, y=90)
-
-b4=Button(root, text="Eliminar Registro",bg="red", command=eliminar)
-b4.place(x=435, y=90)
-
-b5=Button(root, text="Buscar", command=Buscar_jugadores)
-b5.place(x=770, y=87)
-
-root.config(menu=menubar)
-
-root.mainloop()
+if __name__ == '__main__':
+    window = Tk()
+    aplicacion = Jugadores(window)
+    window.mainloop()
